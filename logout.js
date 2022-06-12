731456b801846d5c4101c59916820f32f16c3c9b
Derek Liu <derekliuchiu@berkeley.edu>
2022-06-12 00:45:44 -0700

    added edit info button

---
+++ logout.js
@@ -0,0 +1,3 @@
+document.getElementById("logout").onclick = function () {
+    location.href = "https://card-app.auth.us-west-1.amazoncognito.com/logout?client_id=69ph4qov1jt34e0qfcq3oj9rp4&logout_uri=https://main.d32bwoqy6fkx0b.amplifyapp.com/";
+};
