b0ff180821855129d13e9924014b2e8d0618c110
Derek Liu <derekliuchiu@berkeley.edu>
2022-06-12 00:34:20 -0700

    test

--- editinfo.js
+++ editinfo.js
@@ -1,3 +1,3 @@
 document.getElementById("editinfo").onclick = function () {
-    location.href = "https://main.d32bwoqy6fkx0b.amplifyapp.com/info.html";
+    location.href = "https://main.d32bwoqy6fkx0b.amplifyapp.com/editinfo.html";
 };
