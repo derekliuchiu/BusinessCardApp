e26589f2e1db1fdcde33d254b6a6e99eb598c217
Derek Liu <derekliuchiu@berkeley.edu>
2022-06-11 20:13:33 -0700

    front end almost

---
+++ login.js
@@ -0,0 +1,4 @@
+
+document.getElementById("login").onclick = function () {
+    location.href = "https://card-app.auth.us-west-1.amazoncognito.com/login?response_type=token&client_id=69ph4qov1jt34e0qfcq3oj9rp4&redirect_uri=https://www.example.com";
+};
