dbae73eefb09b1746ecd7098e9914439c89d5c3a
Derek Liu <derekliuchiu@berkeley.edu>
2022-06-11 23:00:53 -0700

    changed second page after sign in

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 
 document.getElementById("login").onclick = function () {
-    location.href = "https://card-app.auth.us-west-1.amazoncognito.com/login?response_type=token&client_id=69ph4qov1jt34e0qfcq3oj9rp4&redirect_uri=https://www.example.com";
+    location.href = "https://card-app.auth.us-west-1.amazoncognito.com/login?response_type=token&client_id=69ph4qov1jt34e0qfcq3oj9rp4&redirect_uri=https://main.d32bwoqy6fkx0b.amplifyapp.com/card.html";
 };
